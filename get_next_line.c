41f724bcc835f7b2d299259e9ccc70235e1f3e84
veryheavypickle <xavier.carroll01@gmail.com>
2022-08-30 01:10:43 +0200

    idk what i did

--- get_next_line.c
+++ get_next_line.c
@@ -4,19 +4,36 @@
  * 0 = False
  * 1 = True
  */
+
+void	test()
+{
+	char	*buf;
+
+	buf = (char *)malloc(sizeof(char) * 1);
+
+	if (!buf)
+		printf("False");
+	else
+		printf("True");
+}
+
 static char	*read_line(int fd)
 {
-	static int	i;
+	static int	i = 0;
 	static char *buf;
-	char		*str;
+	char		*str = NULL;
 
-	str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-
+	//str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	//if (!buf)
 	//buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	//*/
-	if (!i)
-		i = 0;
+	if (!buf)
+	{
+		while (buf[i] != '\n' || buf[i])
+			i++;
+
+	}
 	if (!buf || !str)
 		return (NULL);
 	read(fd, buf, BUFFER_SIZE);
@@ -30,5 +47,6 @@ char	*get_next_line(int fd)
 	//static	int line = 0;
 	if (fd == -1 || BUFFER_SIZE < 1)
 		return (NULL);
+	test();
 	return (read_line(fd));
 }
